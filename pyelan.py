7cf57a5d55cddff3999fbc283a28162361c70ceb
Jonathan Keane <jkeane@gmail.com>
2015-04-19 17:19:26 -0500

    Updated pyelan to allow for multiple media files. This will break compatibiltiy with fflipper.

--- pyelan.py
+++ pyelan.py
@@ -47,7 +47,7 @@ def getRecDirStruct(rootdir):
     for path, dirs, files in os.walk(rootdir):
         [dir.append(os.path.join(path,file)) for file in files]
     return dir
-    
+
 def lcs(string1, string2):
     # from http://stackoverflow.com/questions/5267610/comparing-strings
     m = len(string1)
@@ -60,14 +60,14 @@ def lcs(string1, string2):
                 C[i][j] = C[i - 1][j - 1] + 1
             else:
                 C[i][j] = max(C[i][j - 1], C[i - 1][j])
-    return C[m][n]    
+    return C[m][n]
 
 def findPathMatch(oldPath, searchDir = "./"):
     dirContents = getRecDirStruct(searchDir)
     if os.path.isfile(oldPath) == False:
         # if the current path is not a valid path
         basename = os.path.basename(oldPath)
-        
+
         # filter all of the files in the current directory that match the filename
         newPaths = filter(lambda s: re.match(os.path.sep.join((".*",basename)), s), dirContents)
         if len(newPaths) == 1:
@@ -82,7 +82,7 @@ def findPathMatch(oldPath, searchDir = "./"):
             newPath = oldPath
     else:
         newPath = oldPath
-    return newPath        
+    return newPath
 
 class annotation:
     """A single annotation that has a beginning, an ending, an annotation value, and a unit type (default is milliseconds"""
@@ -94,7 +94,7 @@ class annotation:
         else:
             self.value = value
         self.units = units
- 
+
     def millisToFrames(self, fps = (60.*(1000./1001.))):
         """Converts a tuple (in milliseconds) into frames given a frame rate"""
         secsPerFrame = 1000./fps
@@ -102,12 +102,12 @@ class annotation:
         self.end = int(float(self.end)/secsPerFrame)
         self.units = "frames"
         return self
-    
+
     def framesToMillis(self, fps = (60.*(1000./1001.))):
         """Converts a tuple (in frames) into milliseconds given a frame rate"""
         secsPerFrame = 1000./fps
         # +1 to align with the next frame, not the last one.
-        self.begin = int(float(self.begin)*secsPerFrame+1) 
+        self.begin = int(float(self.begin)*secsPerFrame+1)
         self.end = int(float(self.end)*secsPerFrame+secsPerFrame)
         self.units = "ms"
         return self
@@ -120,9 +120,9 @@ class tier:
 
 class tierSet:
     """A Tier set either from a file, or from media, tiers, and a pathELAN"""
-    def __init__(self, file=None, media=None, linkedFiles=[None], relLinkedFiles=[None], tiers=None, pathELAN=None):
+    def __init__(self, file=None, media=[None], linkedFiles=[None], relLinkedFiles=[None], tiers=None, pathELAN=None):
         if file:
-            tiers,media,linkedFiles,relLinkedFiles = self.extractTiers(file)
+            tiers,media,relMedia,linkedFiles,relLinkedFiles = self.extractTiers(file)
             pathELAN = os.path.dirname(file)
         self.media = media
         self.linkedFiles = linkedFiles
@@ -131,23 +131,26 @@ class tierSet:
         self.pathELAN = pathELAN
 
         ### If media does not exist, try in the same folder as the elan file.:
-        if os.path.isfile(media) == False:
-            sameDirPath = os.path.join(pathELAN,os.path.basename(media))
-            if os.path.isfile(sameDirPath) == False:
-                # self.media = []
-                # self.tiers = tier
-                # self.pathELAN = []
-                warnings.warn("Could not find the media file.")
-                
-            else:    
-                self.media = sameDirPath
+        newMedia = []
+        for mediaFile in media:
+            if os.path.isfile(mediaFile) == False:
+                sameDirPath = os.path.join(pathELAN,os.path.basename(mediaFile))
+                if os.path.isfile(sameDirPath) == False:
+                    # self.mediaFile = []
+                    # self.tiers = tier
+                    # self.pathELAN = []
+                    warnings.warn("Could not find the media file: "+mediaFile)
+                    newMedia = newMedia.append(mediaFile)
+
+                else:
+                    newMedia = newMedia.append(sameDirPath)
 
     def extractTiers(self, file):
         """A function that extracts the tiers from a file and creates a tierSet that includes everything in the file."""
         verbose = False
         tree = ElementTree.parse(file)
         root = tree.getroot()
-        rootLen = len(root)    
+        rootLen = len(root)
         pairs = [(X.attrib['TIME_SLOT_ID'],X.attrib['TIME_VALUE']) for X in root[1]]
         timeDict = dict(pairs)
         if verbose: print(timeDict)
@@ -169,45 +172,55 @@ class tierSet:
         # Find the media file
         # check? <HEADER MEDIA_FILE="" TIME_UNITS="milliseconds">
         header = root.findall('HEADER')
+
+        # Find any media files
         media = header[0].findall('MEDIA_DESCRIPTOR')
-        mediaPath = media[0].attrib['MEDIA_URL']
-        
+        if len(media) > 0:
+            mediaPaths = []
+            relMediaFilePaths = []
+            for mediaFile in media:
+                mediaPaths.append(mediaFile.attrib['MEDIA_URL'][7:]) # [7:] removes the file://
+                if 'RELATIVE_MEDIA_URL' in mediaFile.attrib:
+                    relMediaFilePaths.append(mediaFile.attrib['RELATIVE_MEDIA_URL'])
+        else:
+            mediaPaths = None
+            relMediaFilePaths = None
+
         # Find any linked files
         linkedFiles = header[0].findall('LINKED_FILE_DESCRIPTOR')
         if len(linkedFiles) > 0:
             linkedFilePaths = []
             relLinkedFilePaths = []
             for linkedFile in linkedFiles:
-                linkedFilePaths.append(linkedFile.attrib['LINK_URL'][7:]) # [7:] removes the file://    
+                linkedFilePaths.append(linkedFile.attrib['LINK_URL'][7:]) # [7:] removes the file://
                 if 'RELATIVE_LINK_URL' in linkedFile.attrib:
                     relLinkedFilePaths.append(linkedFile.attrib['RELATIVE_LINK_URL'])
         else:
             linkedFilePaths = None
             relLinkedFilePaths = None
-        
-        # remove "file://" from the path
-        mediaPath = mediaPath[7:]
-        return clipTiers,mediaPath,linkedFilePaths,relLinkedFilePaths
+
+
+        return clipTiers,mediaPaths,relMediaFilePaths,linkedFilePaths,relLinkedFilePaths
 
     def fixLinks(self, searchDir="./"):
         """A function that fixes links in an elan file by searching recursively through the search directory, and then links the best matches for each file."""
-        self.media = os.path.abspath(findPathMatch(self.media, searchDir=searchDir))
+        self.media = [os.path.abspath(findPathMatch(self.media, searchDir=searchDir)) for path in self.media]
         self.linkedFiles = [os.path.abspath(findPathMatch(path, searchDir=searchDir)) for path in self.linkedFiles]
-        
-        
-    
+
+
+
     def selectedTiers(tierObj, tierNames):
         """An unbound function that extracts the tiers given in the list tierNames"""
         media = tierObj.media
         tiers = tierObj.tiers
         pathELAN = tierObj.pathELAN
-        newTiers = []        
+        newTiers = []
         for tr in tiers:
             if tr.tierName in tierNames:
                 newTiers.append(tr)
-        tiers = newTiers    
+        tiers = newTiers
         return tierSet(file=None, media=media, tiers=tiers, pathELAN=pathELAN)
-    
+
     def miniTier(tierObj, begin, end, retimed = True):
         """An unbound function that extracts a subset of a tier"""
         verbose = False
@@ -231,7 +244,7 @@ class tierSet:
                 ## else:
                 ##     print("Skipping, out of bounds.")
             newTiers.append(tier(tierName=newTierName, annotations= newAnnotations))
-        tiers = newTiers    
+        tiers = newTiers
         return tierSet(file=None, media=media, tiers=tiers, pathELAN=pathELAN)
 
     def elanOut(tierObj, headFootFile = os.path.join(os.path.dirname(__file__),"elanSkeleton.eaf"), dest = "./out.eaf"):
@@ -240,16 +253,18 @@ class tierSet:
         tree = ElementTree.parse(headFootFile)
         root = tree.getroot()
         destDir = os.path.dirname(os.path.abspath(dest))
-        
+
         # Set media for the elan file
-        newMedia = tierObj.media
+        mediaFiles = tierObj.media
         tree = ElementTree.ElementTree(root)
         header = root.findall('HEADER')
-        media = ElementTree.SubElement(header[0], 'MEDIA_DESCRIPTOR')
-        media.set('MEDIA_URL', ''.join(["file://",os.path.abspath(newMedia)]))
-        media.set('MIME_TYPE', 'video/*')
-        media.set('RELATIVE_MEDIA_URL', ''.join(["./",os.path.relpath(os.path.abspath(newMedia), destDir)]))
-        
+
+        for mediaFile in mediaFiles:
+            media = ElementTree.SubElement(header[0], 'MEDIA_DESCRIPTOR')
+            media.set('MEDIA_URL', ''.join(["file://",os.path.abspath(mediaFile)]))
+            media.set('MIME_TYPE', 'video/*')
+            media.set('RELATIVE_MEDIA_URL', ''.join(["./",os.path.relpath(os.path.abspath(mediaFile), destDir)]))
+
         # Set the link
         linkedFiles = tierObj.linkedFiles
         for fl in linkedFiles:
@@ -257,47 +272,47 @@ class tierSet:
             media.set('LINK_URL', ''.join(["file://",os.path.abspath(fl)]))
             media.set('MIME_TYPE', 'unknown') # change to read file extension?
             media.set('RELATIVE_LINK_URL', ''.join(["./",os.path.relpath(os.path.abspath(fl), destDir)]))
-        
+
         time_order = root[1]
 
         #----------------------------------------------------
         # build the tiers from the contents of nuTiers
-        #----------------------------------------------------            
+        #----------------------------------------------------
         knownTiers = {}
         tslt = 0
         anot = 0
-        
+
         if tierObj.tiers != None:
             for tier in tierObj.tiers:
                 if verbose: print(tier.tierName)
                 workingTier = tier.tierName
                 #----------------------------------------------------
                 # Create a tier
-                #----------------------------------------------------            
+                #----------------------------------------------------
                 tierNode = ElementTree.SubElement(root, "TIER")
                 tierNode.attrib["ANNOTATOR"] = 'annoCompare'
                 tierNode.attrib["DEFAUTL_LOCALE"] = "en"
                 tierNode.attrib["LINGUISTIC_TYPE_REF"] = "default-lt"
                 tierNode.attrib["TIER_ID"] = workingTier
-            
-                #----------------------------------------------------  
+
+                #----------------------------------------------------
                 for anno in tier.annotations:
                     if verbose: print "Working on time slot: "+str(tslt)+" and annotation: "+str(anot)
-                    tslt += 1 
+                    tslt += 1
                     time_slot_id0 = 'ts' + workingTier + str(tslt)
                     time_value0 = str(anno.begin)
                     time_slot = ElementTree.SubElement(time_order, "TIME_SLOT")
                     time_slot.attrib["TIME_SLOT_ID"] = time_slot_id0
                     time_slot.attrib["TIME_VALUE"] = time_value0
-                    
+
                     tslt += 1
-                    time_slot_id1 = 'ts' + workingTier + str(tslt) 
+                    time_slot_id1 = 'ts' + workingTier + str(tslt)
                     # nx = int(float(nx)*1000)
                     time_value1 = str(anno.end)
                     time_slot = ElementTree.SubElement(time_order, "TIME_SLOT")
                     time_slot.attrib["TIME_SLOT_ID"] = time_slot_id1
                     time_slot.attrib["TIME_VALUE"] = time_value1
-                
+
                     anot += 1
                     annotation_id = 'a' + str(anot)
                     annotation = ElementTree.SubElement(tierNode, "ANNOTATION")
@@ -308,16 +323,16 @@ class tierSet:
 
                     anno_value = ElementTree.SubElement(alignable_annotation, "ANNOTATION_VALUE")
                     anno_value.text = anno.value
-                    
+
                     if verbose: print(anno.value+": "+str(anno.begin)+"-"+str(anno.end))
-        
+
         return tree
-        
+
 def pfsxOut(tsConfigs):
         """An unbound function that returns the pfsx file for the list of TS configs given"""
         verbose = False
 
-        
+
         # Set media for the elan file
         tree = ElementTree.ElementTree()
         root = ElementTree.Element("preferences")
@@ -333,25 +348,25 @@ def pfsxOut(tsConfigs):
                 trackPrefList.set('key', '-'.join([track.split("-")[0], str(nTSes)]))
                 val = ElementTree.SubElement(trackPrefList, 'String')
                 val.text = trackSet[track]
-                
+
         pref = ElementTree.SubElement(root, 'pref')
         pref.set('key', "TimeSeriesViewer.NumberOfPanels")
         int = ElementTree.SubElement(pref, 'Int')
         int.text = str(nTSes)
-        
+
         tree = ElementTree.ElementTree(root)
         # for pretty printing
         root = indent(root)
-                
+
         return tree
-        
-        
+
+
 class track:
     """A track"""
     def __init__(self, name, column, row=0, range=[None, None], color=[0,0,0], properties=None, derivative=0):
         self.name = name
         self.column = column
-        self.row = row    
+        self.row = row
         self.range = range
         self.color = color
         self.properties = properties
@@ -375,17 +390,17 @@ class timeSeries:
         verbose = False
         tree = ElementTree.parse(file)
         root = tree.getroot()
-        rootLen = len(root)    
+        rootLen = len(root)
         # get track source
         tracksource = root.findall("tracksource")[0] # maybe check how many there are. There should only ever be one.
         source = tracksource.attrib['source-url'][7:] # the [7:] removes the file://
         sampleType = tracksource.attrib['sample-type']
         timeCol = int(tracksource.attrib['time-column'])
-        try: 
+        try:
             timeOrigin = float(tracksource.attrib['time-origin'])
         except KeyError:
             timeOrigin = None
-        
+
         # get individual tracks
         tracks = tracksource.findall("track")
         trackList = []
@@ -402,21 +417,21 @@ class timeSeries:
             row = pos.attrib['row']
             range = trk.findall("range")[0] # should be only one
             max = range.attrib['max']
-            min = range.attrib['min']  
+            min = range.attrib['min']
             color = trk.findall("color")[0] # should be only one
             color = [int(i) for i in color.text.split(',')]
-            range = [min,max]  
+            range = [min,max]
             trackList.append(track(name, column, row, range, color, properties, deriv))
         return source, sampleType, trackList, timeCol, timeOrigin
-        
+
     def fixLinks(self, searchDir="./"):
         """A function that fixes links in a tsconf file by searching recursively through the search directory, and then links the best matches for each file."""
         self.source = os.path.abspath(findPathMatch(self.source, searchDir=searchDir))
-        
+
     def timeSeriesOut(tsObj):
         """An unbound function that returns the time series configuration file for the ts object"""
         verbose = False
-        
+
         # Set media for the elan file
         tree = ElementTree.ElementTree()
         root = ElementTree.Element("timeseries")
@@ -429,12 +444,12 @@ class timeSeries:
         tracksource.set('time-column', str(tsObj.timeCol))
         if tsObj.timeOrigin:
             tracksource.set('time-origin', str(tsObj.timeOrigin))
-            
-        
+
+
         prop = ElementTree.SubElement(tracksource, 'property')
         prop.set('key', "provider")
         prop.set('value', "mpi.eudico.client.annotator.timeseries.csv.CSVServiceProvider")
- 
+
         # Set the link
         tracks = tsObj.tracks
         pfsxPrefs = {}
@@ -443,35 +458,32 @@ class timeSeries:
             trck = ElementTree.SubElement(tracksource, 'track')
             trck.set('derivative', str(track.derivative))
             trck.set('name', track.name)
-            
+
             pfsxPrefs['-'.join(['TimeSeriesViewer.Panel',str(pfsxn)])] = track.name
             pfsxn += 1
-            
+
             for propert in track.properties:
                 prop = ElementTree.SubElement(trck, 'property')
                 prop.set('key', propert)
                 prop.set('value', track.properties[propert])
-                
+
             samplePosition = ElementTree.SubElement(trck, 'sample-position')
             pos = ElementTree.SubElement(samplePosition, 'pos')
             pos.set('col', str(track.column))
             pos.set('row', str(track.row))
-            
+
             desc = ElementTree.SubElement(trck, 'description')
             units = ElementTree.SubElement(trck, 'units')
 
             range = ElementTree.SubElement(trck, 'range')
             range.set('max', str(track.range[1]))
-            range.set('min', str(track.range[0]))  
-                      
+            range.set('min', str(track.range[0]))
+
             color = ElementTree.SubElement(trck, 'color')
             color.text = ','.join([str(i) for i in track.color])
-        
+
         tree = ElementTree.ElementTree(root)
         # for pretty printing
         root = indent(root)
-                
-        return tree,pfsxPrefs         
 
- 
-
+        return tree,pfsxPrefs
