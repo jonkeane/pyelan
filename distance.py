95968556a256ab8b21d4862e022d2e85c422a8a3
Jonathan Keane <jkeane@gmail.com>
2017-12-03 16:58:09 -0600

    Added min and max arguments to cohen's kappa

--- distance.py
+++ distance.py
@@ -15,7 +15,7 @@ def match_annos(tier1, tier2, add_blank_for_mismatch = False):
     for anno1, anno2 in itertools.product(tier1.annotations, tier2.annotations):
         if overlap(anno1, anno2):
             matching_annos.append((anno1, anno2))
-            
+
     # add back in non-matches
     if add_blank_for_mismatch:
         gs_to_add = []
@@ -36,7 +36,7 @@ def match_annos(tier1, tier2, add_blank_for_mismatch = False):
                 )))
         matching_annos.extend(gs_to_add)
         matching_annos.extend(comp_to_add)
-                
+
     return(matching_annos)
 
 def overlap(anno1, anno2):
@@ -50,23 +50,23 @@ def overlap(anno1, anno2):
         return(False)
 
 def merge_duplicate_annos(values):
-    # if there are duplicate gc or comp annotations, then collapse the other 
+    # if there are duplicate gc or comp annotations, then collapse the other
     # kind and concatenate. In other words, if one annotatator label 'one two'
-    # but another annotator broke that into two annotations ('one' and 'two') 
+    # but another annotator broke that into two annotations ('one' and 'two')
     # that both overlap the first, then collapse those two into one string.
-    
+
     mergeddict_gc = collections.defaultdict(list)
     for group in values:
-        mergeddict_gc[group[1]].append(group[0])    
+        mergeddict_gc[group[1]].append(group[0])
     gcs = tuple(tuple(mergeddict_gc[k]) for k in [anno[1] for anno in values])
-    
+
     mergeddict_comps = collections.defaultdict(list)
     for group in values:
         mergeddict_comps[group[0]].append(group[1])
     comps = tuple(tuple(mergeddict_comps[k]) for k in [anno[0] for anno in values])
-    
+
     combined_annos = tuple(set(tuple((a, b) for a, b in zip(gcs, comps))))
-    
+
     annos_out = []
     for annopair in combined_annos:
         gc = pyelan.annotation(
@@ -80,15 +80,15 @@ def merge_duplicate_annos(values):
             value = " ".join([anno.value for anno in annopair[1]])
         )
         annos_out.append((gc, comp))
-    
+
     # re-order to be in the correct order
     annos_out.sort(key=lambda x: x[0].begin)
-    
+
     return(annos_out)
 
 def levenshtein(s1, s2):
     """Total Levenshtein distance between two strings
-    
+
     Keyword arguments:
     s1 -- the first string
     s2 -- the second string
@@ -108,144 +108,157 @@ def levenshtein(s1, s2):
 
 def totalLevenshtein(tier1, tier2):
     """Total Levenshtein distance between two tiers
-    
+
     Keyword arguments:
     tier1 -- the first tier (of type pyelan.tier)
     tier2 -- the second tier (of type pyelan.tier)
     """
-    
+
     tier1_string = "".join([anno.value for anno in tier1.annotations])
     tier2_string = "".join([anno.value for anno in tier2.annotations])
-    
+
     n_char = float(max(len(tier1_string), len(tier2_string)))
     return(levenshtein(tier1_string, tier2_string)/n_char)
 
 def overlappingAnnotationLevenshtein(tier1, tier2):
-    """Levenshtein distance between two tiers for only those annotations which 
+    """Levenshtein distance between two tiers for only those annotations which
     overlap
-    
+
     Keyword arguments:
     tier1 -- the first tier (of type pyelan.tier)
     tier2 -- the second tier (of type pyelan.tier)
     """
-    
+
     # iterate over annotations and mark overlaps.
     matching_annos = match_annos(tier1, tier2)
-        
+
     if len(matching_annos) == 0:
         # return None if there are no overlaps
         return(None)
 
     # get strings from annotations
     matching_annos = merge_duplicate_annos(matching_annos)
-    
+
     # join both strings together to determine the number of characters
     tier1_string = "".join([anno[0].value for anno in matching_annos])
     tier2_string = "".join([anno[1].value for anno in matching_annos])
     n_char = float(max(len(tier1_string), len(tier2_string)))
-    
+
     lev_dist = sum([levenshtein(a1.value, a2.value) for a1, a2 in matching_annos])
     return(lev_dist/n_char)
 
-def cohens_kappa(tier1, tier2):
+def cohens_kappa(tier1, tier2, frame_min = None, frame_max = None):
     """Cohen's kappa between two tiers
-    
+
     Keyword arguments:
     tier1 -- the first tier (of type pyelan.tier)
     tier2 -- the second tier (of type pyelan.tier)
+    frame_min -- the lowest bound of frames to check
+    frame_max -- the upper bound of frames to check
     """
     if not sklearn_avail:
         print("Scikit-learn is not available, cohen's kappa can't be calculated.")
         return(None)
-    
+
     # iterate over annotations and mark overlaps.
     matching_annos = match_annos(tier1, tier2, add_blank_for_mismatch = True)
-    
+
     if len(matching_annos) == 0:
         # return None if there are no overlaps
         return(None)
 
     # get strings from annotations
     matching_annos = merge_duplicate_annos(matching_annos)
-    
+
     # change millis to frames
     for a in matching_annos:
         a[0].millisToFrames(fps=29.97)
         a[1].millisToFrames(fps=29.97)
-        
-    min_frame = min([min([a[0].begin, a[1].begin]) for a in matching_annos])
-    max_frame = max([max([a[0].end, a[1].end]) for a in matching_annos])
-    
+
+    if frame_min is None:
+        min_frame = min([min([a[0].begin, a[1].begin]) for a in matching_annos])
+    else:
+        min_frame = min(frame_min,
+                        min([min([a[0].begin, a[1].begin]) for a in matching_annos]))
+
+    if frame_min is None:
+        max_frame = max([max([a[0].end, a[1].end]) for a in matching_annos])
+    else:
+        max_frame = max(frame_max,
+                        max([max([a[0].end, a[1].end]) for a in matching_annos]))
+
     # add GS labels for each frame
-    gs_frames = dict.fromkeys(range(min_frame, max_frame), '')    
+    gs_frames = dict.fromkeys(range(min_frame, max_frame), '')
     for frame in gs_frames:
         for anno in [annos[0] for annos in matching_annos]:
             if frame > anno.begin and frame < anno.end:
                 gs_frames[frame] = anno.value
     # add comp labels for each frame
-    comp_frames = dict.fromkeys(range(min_frame, max_frame), '')    
+    comp_frames = dict.fromkeys(range(min_frame, max_frame), '')
     for frame in comp_frames:
         for anno in [annos[1] for annos in matching_annos]:
             if frame > anno.begin and frame < anno.end:
                 comp_frames[frame] = anno.value
-    
+
     kappa = metrics.cohen_kappa_score(gs_frames.values(), comp_frames.values())
     return(kappa)
 
 def overlappingTimeDiff(tier1, tier2, fps = None):
-    """The sum of the absolute value of the difference of the start and end 
-    anotationsbetween two tiers for only those annotations which 
+    """The sum of the absolute value of the difference of the start and end
+    anotationsbetween two tiers for only those annotations which
     overlap
-    
+
     Keyword arguments:
     tier1 -- the first tier (of type pyelan.tier)
     tier2 -- the second tier (of type pyelan.tier)
     """
-    
+
     # iterate over annotations and mark overlaps.
     matching_annos = match_annos(tier1, tier2)
-        
+
     if len(matching_annos) == 0:
         # return None if there are no overlaps
         return(None)
 
     # get strings from annotations
     matching_annos = merge_duplicate_annos(matching_annos)
-    
+
     if fps is not None:
         # change millis to frames
         for a in matching_annos:
             a[0].millisToFrames(fps=fps)
             a[1].millisToFrames(fps=fps)
-    
+
     # difference between start and end annotations
     start_diff = [abs(anno[0].begin - anno[1].begin) for anno in matching_annos]
     end_diff = [abs(anno[0].end - anno[1].end) for anno in matching_annos]
-    
+
     total_diff = sum(start_diff) + sum(end_diff)
-    
+
     return(total_diff)
 
 
-def compare_files(elanFiles):
+def compare_files(elanFiles, frame_min = None, frame_max = None):
     """Compare files to check reliability
-    
+
     The first tier of the first file is used as the gold standard to compare against.
-    
+
     Keyword arguments:
     elanFiles -- a list of elan files to compare
+    frame_min -- the first frame to check (only used when calculate cohen's kappa)
+    frame_max --the last frame to check (only used when calculate cohen's kappa)
     """
     file_tiers = []
     for fl in elanFiles:
         # suppress media warnings when loading elan files
         with warnings.catch_warnings():
             warnings.simplefilter('ignore', UserWarning)
-            
+
             file_tiers.append({
                 "filename": fl,
                 "tiers": pyelan.tierSet(file = fl)})
     print("Processing {0} files".format(len(file_tiers)))
-    
+
     tiers = []
     for ft in file_tiers:
         fl = ft["filename"]
@@ -256,32 +269,33 @@ def compare_files(elanFiles):
     gs_tier = tiers.pop(0)
     print("Using the tier {0} from file {1} for the gold standard".format(gs_tier["tier"].tierName, gs_tier["filename"]))
     print("The gold-standard tier has {0} annotations".format(len(gs_tier["tier"].annotations)))
-    print("")    
-    
+    print("")
+
     out = []
     for comp_tier in tiers:
         print("Comparing to the tier {0} from file {1}".format(comp_tier["tier"].tierName, comp_tier["filename"]))
         print("The comparison tier has {0} annotations".format(len(comp_tier["tier"].annotations)))
-        
+
         # straight levenshtein distance
         # TODO: calculate GS total characters
         total_lev = totalLevenshtein(gs_tier["tier"], comp_tier["tier"])
         print("There was a total Levenshtein distance for all annotations of: {0}".format(total_lev))
-        
+
         # TODO: calculate GS total characters
         overlap_lev = overlappingAnnotationLevenshtein(gs_tier["tier"], comp_tier["tier"])
         print("There was a Levenshtein distance for overlapping annotations of: {0}".format(overlap_lev))
 
-        kappa = cohens_kappa(gs_tier["tier"], comp_tier["tier"])
+        kappa = cohens_kappa(gs_tier["tier"], comp_tier["tier"],
+                             frame_min = frame_min, frame_max = frame_max)
         print("There was a Cohen's Kappa of: {0}".format(kappa))
-        
+
         time_diff_sum = overlappingTimeDiff(gs_tier["tier"], comp_tier["tier"])
         print("There was a total start/end diff. for overlapping annotations of: {0} seconds".format(time_diff_sum))
 
         frame_diff_sum = overlappingTimeDiff(gs_tier["tier"], comp_tier["tier"], fps = 29.97)
         print("There was a total start/end diff. for overlapping annotations of: {0} frames".format(frame_diff_sum))
         print("")
-        
+
         out.append({
             "gs_file": gs_tier["filename"],
             "gs_tier": gs_tier["tier"].tierName,
@@ -293,9 +307,8 @@ def compare_files(elanFiles):
             "time_diff_sum": time_diff_sum,
             "frame_diff_sum": frame_diff_sum})
     return(out)
-            
-            
+
+
 
 if __name__ == '__main__':
     compare_files(sys.argv[1:])
-
